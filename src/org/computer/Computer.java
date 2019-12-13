be571d2cc0050a8418dc63ce09ce2fecd81090d5
arpitha75 <arpithar75@gmail.com>
2019-12-13 09:49:14 +0530

    conflict one at arun branch

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -3,5 +3,6 @@ package org.computer;
 public class Computer {
 public void computerModel() {
 	System.out.println("Computer Model is Ios ");
+	System.out.println("conflicts number one at arun branch");
 }
 }
