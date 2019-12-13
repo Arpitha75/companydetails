3666b069ecf0d010f67b3dac793cd67a33207762
arpitha75 <arpithar75@gmail.com>
2019-12-13 20:03:29 +0530

    conflict number three

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -5,5 +5,8 @@ public void computerModel() {
 	System.out.println("Computer Model is Ios ");
 	System.out.println("hello");
 	System.out.println("conflicts number one at arun branch");
+	System.out.println("conflicts number three at arun branch");
+	System.out.println("second line of conflict number three");
+	System.out.println("third line of conflict number three");
 }
 }
