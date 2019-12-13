848df47623ff3d038748e474e73dd9e3d09b56eb
arpitha75 <arpithar75@gmail.com>
2019-12-13 19:49:39 +0530

    conflict done two line at arpitha branch

--- src/org/computer/Computer.java
+++ src/org/computer/Computer.java
@@ -4,6 +4,9 @@ public class Computer {
 public void computerModel() {
 	System.out.println("Computer Model is Ios ");
 	System.out.println("hello");
+	System.out.println("conflict number two at arpitha branch");
+	System.out.println("second line of conflict number two at arpitha branch");
+	
 	
 }
 }
